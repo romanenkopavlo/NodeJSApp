18c60fbd11b8d3bbe4dbc5c8c76aecc81b31ccfd
pavlo <paihitetty444@gmail.com>
2024-04-11 09:56:20 +0200

    Le debut

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,26 @@
 var express = require('express');
-
 var router = express.Router();
-let compte = {};
 
 router.get('/', function(req, res, next) {
-  if ( req.session.compte == null) {
+  if (req.session.compte == null) {
     console.log(req.session.id);
     res.render('index', {title: '', compte: null});
   } else {
     res.render('index', {title: '', compte: req.session.compte});
   }
+});
 
+router.get('/inscription', function (req, res, next) {
+  res.render('inscription', {title: "inscription ",compte : null});
+});
+
+router.post('/connexion', function (req, res, next) {
+  req.session.compte = {
+    'nom': req.body.nom,
+    'prenom': req.body.prenom,
+    'login': req.body.login,
+    'password': req.body.password,
+  };
+  res.render('suite', {title: "Connexion", compte: req.session.compte})
 });
 module.exports = router;
